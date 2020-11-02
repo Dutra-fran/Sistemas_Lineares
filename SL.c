a1c07c52bb01e78f2f6ef4cfaec64f757089defc
Dutra-fran <63562657+Dutra-fran@users.noreply.github.com>
2020-11-02 08:48:32 -0300

    Update SL.c
    Fazendo a documentação do código e melhorando a codificação do mesmo.

--- SL.c
+++ SL.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-  int x, y, z;
-  int cont;
-  int sair;
-  int igual1, igual2, igual3;
-  int co1, co2, co3, co4, co5, co6, co7, co8, co9;
-  float x1, y1, z1;
+  // 
+  int x, y, z; // variáveis que carregarão os valores corretos dos sistemas informado.
+  int cont; // variável responsável pela contagem do loop while.
+  int sair; // Não tem muita utilidade, além de somente ser a última função do programa para sair do programa.
+  int igual1, igual2, igual3; // variáveis que carregarão os valores de igualdade dos sistemas. Ex: 3X + Y - 2Z = 7; Esse número 7, seria o nosso número de igualdade.
+  int co1, co2, co3, co4, co5, co6, co7, co8, co9; // variáveis relacionadas aos coeficientes das incógnitas. Ex: 2X; neste caso, o número 2 é coeficiente de X.
+  int x_resp, y_resp, z_resp;
+  int sistema1, sistema2, sistema3; // variáveis que carregarão valores dos sistemas de forma simplificada lá no loop while.
 
+  cont = 1000000; // O loop while fará no máximo 1.000.000 de repetições
+  x = -100; // o resultado negativo de X poderá ser, no máximo, até -100.
+  y = -100; // o resultado negativo de Y poderá ser, no máximo, até -100.
+  z = -100; // o resultado negativo de Z poderá ser, no máximo, até -100.
+  
   co1=0;
   co2=0;
   co3=0;
@@ -18,103 +25,104 @@ int main(){
   co8=0;
   co9=0;
 
-  printf("Sejam bem vindos ao programa que calcula sistemas lineares! :v\n");
-  printf("\n\nAviso: Este programa so' trabalha com resultados inteiros!");
+  printf("Sejam bem-vindos ao programa que calcula sistemas lineares! :v\n");
+  printf("\n\nAviso: este programa so' trabalha com resultados inteiros!");
+
   printf("\n------------------------");
   printf("\nSeja os sistemas lineares com forma:");
-  printf("\n\t1. x + y + z");
-  printf("\n\t2. x + y + z");
-  printf("\n\t3. x + y + z");
-  printf("\n\nOBS: Caso alguma variavel for negativa, e' so colocar o sinal do coeficiente da variavel como negativo!\n");
+  printf("\n\t1. X + Y + Z");
+  printf("\n\t2. X + Y + Z");
+  printf("\n\t3. X + Y + Z");
+  printf("\n\nOBS: Caso alguma variavel for negativa, e' so' colocar o sinal do coeficiente da variavel como negativo!\n");
   printf("\n------------------------");
+
   printf("\n\nPreencha as informacoes do Sistema 1: ");
   printf("\nA variavel X tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co1);
+  scanf("%d", &co1); // Recebe o valor do usuário e armazena na variável co1.
   printf("A variavel Y tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co2);
+  scanf("%d", &co2); // Recebe o valor do usuário e armazena na variável co2.
   printf("A variavel Z tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co3);
+  scanf("%d", &co3); // Recebe o valor do usuário e armazena na variável co3.
   printf("O sistema 1 e' igual a que numero: ");
-  scanf("%d", &igual1);
+  scanf("%d", &igual1); // Recebe o valor da igualdade, informada pelo usuário, e armazena na variável igual1.
 
   printf("\nPreencha as informacoes do Sistema 2: ");
   printf("\nA variavel X tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co4);
+  scanf("%d", &co4); // Recebe o valor do usuário e armazena na variável co4.
   printf("A variavel Y tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co5);
+  scanf("%d", &co5); // Recebe o valor do usuário e armazena na variável co5.
   printf("A variavel Z tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co6);
-  printf("O sistema 2 e' igual a que numero: ");
-  scanf("%d", &igual2);
+  scanf("%d", &co6); // Recebe o valor do usuário e armazena na variável co6.
+  printf("O sistema 2 e' igual a que numero: "); 
+  scanf("%d", &igual2); // Recebe o valor da igualdade, informada pelo usuário, e armazena na variável igual2.
+
   printf("\n\nPreencha as informacoes do Sistema 3: ");
   printf("\nA variavel X tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co7);
+  scanf("%d", &co7); // Recebe o valor do usuário e armazena na variável co7.
   printf("A variavel Y tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co8);
+  scanf("%d", &co8); // Recebe o valor do usuário e armazena na variável co8.
   printf("A variavel Z tem algum coeficiente? (Caso nao tenha insira 1): ");
-  scanf("%d", &co9);
+  scanf("%d", &co9); // Recebe o valor do usuário e armazena na variável co9.
   printf("O sistema 3 e' igual a que numero: ");
-  scanf("%d", &igual3);
+  scanf("%d", &igual3); // Recebe o valor da igualdade, informada pelo usuário, e armazena na variável igual3.
+
+  // Mostrando ao usuário como ficaram os sistemas lineares.
   printf("\n\tAtualizando os sistemas pra uma forma mais legivel e visivel para o Usuario: ");
   printf("\nSistema 1: (%d * X) + (%d * Y) + (%d * Z) = %d", co1, co2, co3, igual1);
   printf("\nSistema 2: (%d * X) + (%d * Y) + (%d * Z) = %d", co4, co5, co6, igual2);
   printf("\nSistema 3: (%d * X) + (%d * Y) + (%d * Z) = %d", co7, co8, co9, igual3);
   printf("\n------------------------");
 
-  cont = 1000000;
-  x = -100;
-  y = -100;
-  z = -100;
-  float sistema1, sistema2, sistema3, sistema4, sistema5, sistema6, sistema7, sistema8, sistema9;
-
   while(cont > 0){
-    x++;
-    sistema1 = co1 * x;
-    sistema2 = co2 * y;
-    sistema3 = co3 * z;
-    sistema4 = co4 * x;
-    sistema5 = co5 * y;
-    sistema6 = co6 * z;
-    sistema7 = co7 * x;
-    sistema8 = co8 * y;
-    sistema9 = co9 * z;
+    x++; // toda vez que esse laço for se repetindo, essa variável irá ficar incrementando-se +1.
+    sistema1 = (co1 * x) + (co2 * y) + (co3 * z); // Valor do primeiro sistema.
+    sistema2 = (co4 * x) + (co5 * y) + (co6 * z); // Valor do segundo sistema.
+    sistema3 = (co7 * x) + (co8 * y) + (co9 * z); // Valor do terceiro sistema.
 
-    if((sistema1 + sistema2 + sistema3 == igual1 && sistema4 + sistema5 + sistema6 == igual2) && sistema7 + sistema8 + sistema9 == igual3){
-      cont = 0;
-      x1 = x;
-      y1 = y;
-      z1 = z;
+    switch(x){
+      case 451:
+        // Caso o X chegue no valor 451, esse bloco de códigos irá executar as instruções abaixo:
+        x = -99;
+        y++; // a variável Y será incrementado.
+        break;
     }
 
     switch(y){
       case 451:
-        y = -99;
-        x++;
-        z++;
-        break;
-    }
-
-    switch(x){
-      case 451:
-        x = -99;
-        y++;
+        // Caso o Y chegue no valor 451, esse bloco de códigos irá executar as instruções abaixo:
+        y = -99; // a variável Y receberá o valor de -99.
+        z++; // a variável Z será incrementado.
         break;
     }
 
     switch(z){
       case 451:
-        z=-99;
+        // Caso o Z chegue no valor 451, esse bloco de códigos irá executar as instruções abaixo:
+        z=-99; // a variável Z receberá o valor de -99
         break;
+    }
+
+    if((sistema1 == igual1 && sistema2 == igual2) && sistema3 == igual3){
+      // Se os sistemas forem iguais às suas respectivas igualdades, esse laço condicional irá ser executado.
+      cont = 0; // a variável de contagem receberá valor 0, para que o laço de repetição se encerre.
+
+      // Atribuição de respostas.
+      x_resp = x;
+      y_resp = y;
+      z_resp = z;
     }
   }
 
   printf("\n\n\tValor das variaveis: ");
-  printf("\nX = %1.0f", x1);
-  printf("\nY = %1.0f", y1);
-  printf("\nZ = %1.0f", z1);
+  printf("\nX = %d", x_resp);
+  printf("\nY = %d", y_resp);
+  printf("\nZ = %d", z_resp);
+
   printf("\n------------------------");
-  printf("\n\tPrograma feito por Robson Filho! xD");
+  printf("\n\tPrograma feito por Robson Dutra! xD");
+
   printf("\n\nSelecione qualquer caractere do seu Teclado e em Seguida aperte ENTER para sair: ");
-  scanf("%d", &sair);
-  return 0;
+  scanf("%d", &sair); // esse valor recebido não servirá de nada. Esse scanf, serve apenas para que no final do programa, ele aguarde o usuário digitar algo para se encerrar.
+
+  return 0; // se o programa chegou até aqui sem erros, irá retornar 0.
 }
